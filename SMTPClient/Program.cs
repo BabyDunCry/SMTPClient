d4bca49c6f140e15f536a468f2637ef256c96ce8
KelvinChuaJX <kelvin.chua@ghl.com>
2023-02-10 15:07:49 +0800

    First commit

---
+++ SMTPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SMTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var tete = "";
+                var senderEmail = "";
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(senderEmail);
+                mail.To.Add("kelvincjx@gmail.com");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(senderEmail, tete);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("mail Send");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
